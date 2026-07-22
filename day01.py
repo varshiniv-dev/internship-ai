47ab80d3f4bd93674e4a3e94859fc7876a521319
varshiniv-dev <varshiniv.dev@gmail.com>
2026-07-22 10:57:48 +0530

    Added Day 1

---
+++ day01.py
@@ -0,0 +1,35 @@
+# 1. How to print?
+print("Hello, From Varsh")
+
+# 2. How to know which data type the statement is in?
+print("Learning this is through GlowLogics.")
+print("21-07-2026")
+print(19.7)
+print(type("Learning this is through GlowLogics."))
+print(type(21072026))
+print(type(19.7))
+
+# 3. Variables, assigning a variable using '='
+
+name = ("Varsh")
+age = 19.7
+print("Name: ", name)
+print("Age:", age)
+
+# 4. Simple assignment
+name = ("Varsh")
+age = 19.7
+dept = ("CSE")
+college = ("G Madegowda Institute of Technology")
+state = ("Karnataka")
+height = ("5.6foot")
+hobby = ("Drawing")
+series = ("Outlander S1")
+print("Name: ", name)
+print("Age:", age)
+print("Dept: ", dept)
+print("College: ", college)
+print("State:", state)
+print("Height: ", height)
+print("Main Hobby: ", hobby)
+print("Recent series watched: ", series)
